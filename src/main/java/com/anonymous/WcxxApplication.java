1fb87a93731bf7da577b61c04c948edbcae41e90
WangZK <13434995010@163.com>
2017-05-25 13:16:06 +0800

    提交：项目启动类

---
+++ src/main/java/com/anonymous/WcxxApplication.java
@@ -0,0 +1,12 @@
+package com.anonymous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcxxApplication.class, args);
+	}
+}
