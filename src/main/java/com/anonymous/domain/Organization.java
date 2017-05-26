8a12f3fc5a712f239ebcb5df6cdc1d80fd56a0db
WangZK <13434995010@163.com>
2017-05-26 11:30:40 +0800

    提交：组织机构实体类提交

---
+++ src/main/java/com/anonymous/domain/Organization.java
@@ -0,0 +1,50 @@
+package com.anonymous.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by WangZK on 2017/5/25.
+ * 组织机构实体类
+ */
+@Entity
+public class Organization implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "UUID2_GENERATOR")
+    private String id;//组织机构id
+
+    private String name;//组织机构名称
+
+    private String pid;//上级的组织机构id
+
+    public Organization() {
+    }
+
+    public Organization(String organization_id) {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+}
