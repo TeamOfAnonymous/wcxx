63dd49cdfa1e46af158d20324ba6d97a72c150c4
WangZK <13434995010@163.com>
2017-05-26 11:31:01 +0800

    提交：用户实体类提交

---
+++ src/main/java/com/anonymous/domain/User.java
@@ -0,0 +1,106 @@
+package com.anonymous.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * Created by WangZK on 2017/5/25.
+ * 用户实体类
+ */
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "UUID2_GENERATOR")
+    private String id;//用户id
+    private String username;//用户名
+    private String password;//密码
+    private String name;//姓名
+    private String sex;//性别
+    private String post;//职务
+    private String phoneNumber;//联系电话
+    private String email;//邮件地址
+
+    @ManyToOne
+    private Organization organization; //所在部门
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+}
