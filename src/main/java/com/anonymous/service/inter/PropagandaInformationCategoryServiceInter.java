5320decbbfb8bbb7f4f2ea67f955d1fea7fb611f
huangMP <675454967@qq.com>
2017-06-01 15:29:32 +0800

    添加：添加宣传信息类别管理Service层接口

---
+++ src/main/java/com/anonymous/service/inter/PropagandaInformationCategoryServiceInter.java
@@ -0,0 +1,11 @@
+package com.anonymous.service.inter;
+
+import com.anonymous.domain.Organization;
+import com.anonymous.domain.PropagandaInformationCategory;
+
+/**
+ * Created by WangZK on 2017/6/1.
+ */
+public interface PropagandaInformationCategoryServiceInter {
+    PropagandaInformationCategory add(PropagandaInformationCategory propagandaInformationCategory);
+}
