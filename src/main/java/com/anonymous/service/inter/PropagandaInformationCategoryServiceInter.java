4a95e263f03e8d4b3913513fb683e389efd4f63f
peivuxan <944184696@qq.com>
2017-06-06 11:14:44 +0800

    未成品

--- src/main/java/com/anonymous/service/inter/PropagandaInformationCategoryServiceInter.java
+++ src/main/java/com/anonymous/service/inter/PropagandaInformationCategoryServiceInter.java
@@ -4,6 +4,8 @@ import com.anonymous.domain.Organization;
 import com.anonymous.domain.PropagandaInformationCategory;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by WangZK on 2017/6/1.
@@ -17,7 +19,25 @@ public interface PropagandaInformationCategoryServiceInter {
 
     PropagandaInformationCategory findById(String propagandaInformationCategory_id);
 
-    List<PropagandaInformationCategory> find();
+    List<PropagandaInformationCategory> findByPidIsNull();
 
     List<PropagandaInformationCategory> findByPid(String pid);
+
+    /**
+     * 通过模糊字段查询
+     * @param indistinct 模糊字段
+     * @return
+     */
+    Set<List<PropagandaInformationCategory>> findByIndistinctName(String indistinct);
+
+    /**
+     * 将数据显示出来
+     */
+    Set<List<PropagandaInformationCategory>> displayAll();
+
+    /**
+     * 将各分类数据封装成map
+     */
+    Set<List<PropagandaInformationCategory>> packageListToMap(List<PropagandaInformationCategory> propagandaInformationCategories);
+
 }
