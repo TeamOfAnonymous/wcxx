931d521ccfc6a9d2f65b9129d0762b35856f9175
huangMP <675454967@qq.com>
2017-06-08 10:40:55 +0800

    添加方法     1. 通过 开始日期 和 最后日期 查询 宣传品

--- src/main/java/com/anonymous/repository/PropagandaMaterialsProducedRepository.java
+++ src/main/java/com/anonymous/repository/PropagandaMaterialsProducedRepository.java
@@ -1,8 +1,17 @@
 package com.anonymous.repository;
 
-import com.anonymous.domain.PropagandaMaterialsProduced;
+import com.anonymous.domain.PropagandaMaterialsProduced.ApprovalStatus;
+import com.anonymous.domain.PropagandaMaterialsProduced.ProductionProducedMethod;
+import com.anonymous.domain.PropagandaMaterialsProduced.PropagandaMaterialsProduced;
+import com.anonymous.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by huangMP on 2017/5/26.
@@ -10,4 +19,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PropagandaMaterialsProducedRepository extends JpaRepository<PropagandaMaterialsProduced, String> {
+
+    /**
+     * 通过 开始日期 和 最后日期 查询 宣传品
+     * @param applicationDateStart
+     * @param applicationDateEnd
+     * @return
+     */
+    List<PropagandaMaterialsProduced> findByApplicationDateBetween(LocalDateTime applicationDateStart ,LocalDateTime applicationDateEnd );
+
 }
+
