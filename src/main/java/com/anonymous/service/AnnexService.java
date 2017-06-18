ad4c7885b3ff92e94f390ed83898c294a27a70e2
WangZK <13434995010@163.com>
2017-06-18 14:07:28 +0800

    添加附件的功能

---
+++ src/main/java/com/anonymous/service/AnnexService.java
@@ -0,0 +1,57 @@
+package com.anonymous.service;
+
+import com.anonymous.domain.Annex;
+import com.anonymous.repository.AnnexRepository;
+import com.anonymous.service.inter.AnnexServiceInter;
+import com.anonymous.util.UuidUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by WangZK on 2017/6/18.
+ */
+@Service
+public class AnnexService implements AnnexServiceInter {
+
+    //附件的相对路径文件夹
+    @Value("${annexUrl}")
+    private String annexUrl;
+
+    @Autowired
+    private AnnexRepository annexRepository;
+
+    @Override
+    public Annex saveAnnex(MultipartFile file) {
+
+        File newFile = new File(ClassUtils.getDefaultClassLoader().getResource("").getPath() + "static/" + annexUrl);
+        //判断文件夹是否存在，如果不存在创建文件夹
+        if (!newFile.exists()) {
+            newFile.mkdir();
+        }
+        String originalFileName = file.getOriginalFilename();
+        String newFileName = UuidUtil.get32UUID() + getSuffix(originalFileName);
+        newFile = new File(ClassUtils.getDefaultClassLoader().getResource("").getPath() + "static/" + annexUrl + newFileName);
+        Annex annex = new Annex();
+        try {
+            file.transferTo(newFile);
+            annex.setName(originalFileName);
+            annex.setPath(annexUrl + newFileName);
+            annex = annexRepository.save(annex);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return annex;
+    }
+
+    //获取文件后缀名
+    private String getSuffix(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+}
---
+++ src/main/java/com/anonymous/service/inter/AnnexServiceInter.java
@@ -0,0 +1,11 @@
+package com.anonymous.service.inter;
+
+import com.anonymous.domain.Annex;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by WangZK on 2017/6/18.
+ */
+public interface AnnexServiceInter {
+    Annex saveAnnex(MultipartFile file);
+}
