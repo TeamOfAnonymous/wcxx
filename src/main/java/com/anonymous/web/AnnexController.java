03c620ec2bbbd47fb0aca1bd7f09558ff051b177
WangZK <13434995010@163.com>
2017-06-20 17:37:39 +0800

    修改附件上传

--- src/main/java/com/anonymous/web/AnnexController.java
+++ src/main/java/com/anonymous/web/AnnexController.java
@@ -2,12 +2,16 @@ package com.anonymous.web;
 
 import com.anonymous.domain.Annex;
 import com.anonymous.service.inter.AnnexServiceInter;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.spring.web.json.Json;
 
 /**
  * Created by WangZK on 2017/6/18.
@@ -22,7 +26,10 @@ public class AnnexController {
     @RequestMapping(value = "uploadAnnex", method = RequestMethod.POST)
     @ApiOperation(value = "添加附件")
     @ResponseBody
-    public Annex uploadAnnex(@RequestParam MultipartFile file) {
-        return annexService.saveAnnex(file);
+    public String uploadAnnex(@RequestParam MultipartFile file) throws JsonProcessingException {
+        Annex annex = annexService.saveAnnex(file);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = objectMapper.writeValueAsString(annex);
+        return jsonStr;
     }
 }
